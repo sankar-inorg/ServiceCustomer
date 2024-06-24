c35ef23cfedb28a88b1a988503e753b8e3d5f74f
Riya-InOrg <riya.k.goyal@inorg.com>
2024-06-24 23:14:27 +0530

    Add files via upload

--- src/main/java/com/inorg/services/product/service/CustomerService.java
+++ src/main/java/com/inorg/services/product/service/CustomerService.java
@@ -1,18 +1,33 @@
 package com.inorg.services.product.service;
 
 import com.commercetools.api.models.customer.Customer;
+import com.commercetools.api.models.customer.CustomerPagedQueryResponse;
 import com.commercetools.api.models.customer.CustomerSignInResult;
 import com.commercetools.api.models.customer.CustomerToken;
-import com.inorg.services.product.com.inorg.services.product.models.CustomerData;
+import com.commercetools.api.models.customer_group.CustomerGroup;
+import com.inorg.services.product.models.CustomerData;
 
 public interface CustomerService {
-    public CustomerSignInResult createNewCustomer(CustomerData customerData);
+
+    CustomerSignInResult createNewCustomer(CustomerData customerData);
 
     CustomerSignInResult loginCustomer(CustomerData customerData);
 
-    CustomerToken createRestPasswordToken(String customerId);
+    CustomerToken createResetPasswordToken(String customerId);
 
     Customer resetPassword(String token, String newPassword);
 
+    CustomerToken createEmailVerificationToken(String customerId);
 
+    Customer verifyEmailToken(String token);
+
+    Customer verifyEmail(String token);
+
+    CustomerGroup createCustomerGroup(String customerGroupName);
+
+    Customer addCustomerToACustomerGroup(String customerId, String customerGroupId);
+
+    Customer updateCustomerShoeSize(String customerKey, String shoeSize);
+
+    CustomerPagedQueryResponse getCustomerByShoeSize(String preferredShoeSize);
 }
--- src/main/java/com/inorg/services/product/service/CustomerServiceImpl.java
+++ src/main/java/com/inorg/services/product/service/CustomerServiceImpl.java
@@ -1,19 +1,21 @@
 package com.inorg.services.product.service;
 
 import com.commercetools.api.client.ProjectApiRoot;
-import com.commercetools.api.models.customer.Customer;
-import com.commercetools.api.models.customer.CustomerCreatePasswordResetTokenBuilder;
-import com.commercetools.api.models.customer.CustomerDraft;
-import com.commercetools.api.models.customer.CustomerDraftBuilder;
-import com.commercetools.api.models.customer.CustomerResetPasswordBuilder;
-import com.commercetools.api.models.customer.CustomerSignInResult;
-import com.commercetools.api.models.customer.CustomerSigninBuilder;
-import com.commercetools.api.models.customer.CustomerToken;
-import com.inorg.services.product.com.inorg.services.product.models.CustomerData;
+import com.commercetools.api.models.common.LocalizedStringBuilder;
+import com.commercetools.api.models.customer.*;
+import com.commercetools.api.models.customer_group.CustomerGroup;
+import com.commercetools.api.models.customer_group.CustomerGroupDraftBuilder;
+import com.commercetools.api.models.customer_group.CustomerGroupResourceIdentifierBuilder;
+import com.commercetools.api.models.type.FieldContainerBuilder;
+import com.commercetools.api.models.type.TypeResourceIdentifierBuilder;
+import com.inorg.services.product.models.CustomerData;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -33,9 +35,8 @@ public class CustomerServiceImpl implements CustomerService {
                 .firstName(customerData.getFirstName())
                 .lastName(customerData.getLastName())
                 .password(customerData.getPassword())
-                .key(customerData.getCustomerKey())
+                .key(customerData.getKey())
                 .build();
-
         return apiRoot.customers()
                 .post(customerDraft)
                 .executeBlocking()
@@ -54,15 +55,12 @@ public class CustomerServiceImpl implements CustomerService {
     }
 
     @Override
-    public CustomerToken createRestPasswordToken(String customerId) {
+    public CustomerToken createResetPasswordToken(String customerId) {
         Customer customer = apiRoot.customers()
                 .withId(customerId)
                 .get()
                 .executeBlocking()
                 .getBody();
-
-
-
         return apiRoot.customers()
                 .passwordToken()
                 .post(CustomerCreatePasswordResetTokenBuilder.of()
@@ -73,7 +71,6 @@ public class CustomerServiceImpl implements CustomerService {
                 .getBody();
     }
 
-
     @Override
     public Customer resetPassword(String token, String newPassword) {
         return apiRoot.customers()
@@ -82,6 +79,116 @@ public class CustomerServiceImpl implements CustomerService {
                         .tokenValue(token)
                         .newPassword(newPassword)
                         .build())
+                .executeBlocking()
+                .getBody();
+    }
+
+    @Override
+    public CustomerToken createEmailVerificationToken(String customerId){
+        return apiRoot.customers()
+                .emailToken()
+                .post(CustomerCreateEmailTokenBuilder.of()
+                        .id(customerId)
+                        .ttlMinutes(60L)
+                        .build())
+                .executeBlocking()
+                .getBody();
+    }
+
+    @Override
+    public Customer verifyEmailToken(String token) {
+        return apiRoot.customers()
+                .withEmailToken(token)
+                .get()
+                .executeBlocking()
+                .getBody();
+    }
+
+    @Override
+    public Customer verifyEmail(String token) {
+        return apiRoot.customers()
+                .emailConfirm()
+                .post(CustomerEmailVerifyBuilder.of()
+                        .tokenValue(token).build())
+                .executeBlocking()
+                .getBody();
+    }
+
+    @Override
+    public CustomerGroup createCustomerGroup(String customerGroupName) {
+        return apiRoot.customerGroups()
+                .post(CustomerGroupDraftBuilder.of()
+                        .groupName(customerGroupName)
+                        .build())
+                .executeBlocking()
+                .getBody();
+    }
+
+    @Override
+    public Customer addCustomerToACustomerGroup(String customerId, String customerGroupId) {
+        // Get the Customer
+        Customer customer = apiRoot.customers()
+                .withId(customerId)
+                .get()
+                .executeBlocking().getBody();
+        // Create an Action List
+        List<CustomerUpdateAction> customerUpdateActionList = new ArrayList<>();
+        // Create an Action
+        CustomerUpdateAction setCustomerGroup = CustomerSetCustomerGroupActionBuilder.of()
+                .customerGroup(CustomerGroupResourceIdentifierBuilder.of()
+                        .id(customerGroupId)
+                        .build())
+                .build();
+        // Add the Action to the Action List
+        customerUpdateActionList.add(setCustomerGroup);
+        // Create the Draft using the Version from Customer and the Action List
+        CustomerUpdate customerUpdate = CustomerUpdateBuilder.of()
+                .version(customer.getVersion())
+                .actions(customerUpdateActionList)
+                .build();
+        // Pass the JSON Body and Make the API Call
+        return apiRoot.customers()
+                .withId(customerId)
+                .post(customerUpdate)
+                .executeBlocking()
+                .getBody();
+    }
+
+    @Override
+    public Customer updateCustomerShoeSize(String customerKey, String shoeSize) {
+        // Get the Customer
+        Customer customer = apiRoot.customers()
+                .withKey(customerKey)
+                .get()
+                .executeBlocking()
+                .getBody();
+        // Create the Update Action
+        CustomerUpdateAction updateAction = CustomerSetCustomTypeActionBuilder.of()
+                .type(TypeResourceIdentifierBuilder.of()
+                        .key("customer-preferredShoeSize")
+                        .build())
+                .fields(FieldContainerBuilder.of()
+                        .addValue("preferredShoeSize", LocalizedStringBuilder.of()
+                                .addValue("en-US", shoeSize).build())
+                        .build())
+                .build();
+        // Make a Call to Update API
+        return apiRoot.customers()
+                .withId(customer.getId())
+                .post(CustomerUpdateBuilder.of()
+                        .version(customer.getVersion())
+                        .actions(updateAction)
+                        .build())
+                .executeBlocking()
+                .getBody();
+    }
+
+    @Override
+    public CustomerPagedQueryResponse getCustomerByShoeSize(String preferredShoeSize) {
+        // Using Where Clause
+        return apiRoot.customers()
+                .get()
+                .withWhere("custom(fields(preferredShoeSize(en-US = \"" + preferredShoeSize + "\")))")
                 .executeBlocking()
                 .getBody();
     }
